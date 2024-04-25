b8ab977b97b614821ed0345d44fbc818faed9503
Martin  Stoyanov <martotbb.2556@gmail.com>
2024-04-25 17:44:46 +0300

    updated automata header

--- automata.h
+++ automata.h
@@ -2,12 +2,30 @@
 #include"State.h"
 #include"deltaRelation.h"
 #include<vector>
+#include<string>
+class Automata{
+    public:
+    Automata(unsigned _id,std::vector<State> _states,std::vector<DeltaRelation> _edges);
+    Automata(std::string regex);
 
-class Aoutomata{
+    unsigned getId()const;
+    
+    void Print() const;
+    void Save()const;
+    bool Empty()const;
+    bool Finite()const;
+    bool Deterministic()const;
+    bool Recognize(std::string word)const;
+    Automata& Union(const Automata &other)const;
+    Automata& Concat(const Automata &other)const;
+    Automata& Un(const Automata &other)const;
+    
 
 
     private:
     unsigned int id;
     std::vector<State>states;
     std::vector<DeltaRelation>edges;
+
+    std::vector<State> getUniqueStates(const Automata&other)const;
 };
